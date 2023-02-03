8320df9cd92aa6a3aa34fba71f4e474b42761ad4
Klochko A.S <amigo87@bk.ru>
2023-02-04 00:19:19 +0700

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number' || typeof b !== 'number'){
+    throw new TypeError('Один или оба аргумента не являются числами');
+  }
+  return a + b;
 }
 
 module.exports = sum;
