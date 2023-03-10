2145374e064bb75af249df37342cba9d66dd5d0b
Klochko A.S <amigo87@bk.ru>
2023-03-10 22:45:47 +0700

    task 7.1

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,31 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User')
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+
+      if (false == /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(email)){
+        done(null, false, 'Введён некорректный email')
+        return
+      }
+      if(password.length == 0){
+        done(null, false, 'Вы забыли заполнить пароль')
+        return
+      }
+      const user = await User.findOne({email: email})
+
+      if (!user){
+        done(null, false, 'Нет такого пользователя')
+        return
+      }
+      const result = await user.checkPassword(password)
+
+      if (!result){
+        done(null, false, 'Неверный пароль')
+        return
+      }
+
+      done(null, user);
     },
 );
